be443b9aa45a3a5f82c965e6dcbb19e724448fcc
Isfar Oshir <iao233@nyu.edu>
2021-10-20 17:18:39 -0400

    react-setup

---
+++ front-end/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import {BrowserRouter as Router,
+Route,
+Switch} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <h1>Home</h1>
+          </Route>
+          <Route path='/other' exact>
+            <h1>Other</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
