b83250e9aa28f3edf53078a74594988ada544aa5
Isfar Oshir <iao233@nyu.edu>
2021-10-20 20:33:24 -0400

    react-setup-styling

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router,
-Route,
-Switch} from 'react-router-dom'
+import {BrowserRouter as Router,Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
