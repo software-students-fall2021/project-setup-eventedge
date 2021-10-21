bfdde17432336bc5a13d26e20770670b25666f10
nedjulius <julius.nedzinskas@gmail.com>
2021-10-21 17:22:24 -0400

    Format

--- front-end/src/components/navigation/navigation.jsx
+++ front-end/src/components/navigation/navigation.jsx
@@ -6,14 +6,20 @@ import styles from './navigation.module.css';
 export const Navigation = () => {
   const [isMenuVisible, setIsMenuVisible] = useState(false);
 
-  const toggleIsMenuVisible = () => setIsMenuVisible(prev => !prev);
+  const toggleIsMenuVisible = () => setIsMenuVisible((prev) => !prev);
 
-  return <>
-    <nav className={styles.navigationStrip}>
-      <button className={styles.burgerButton} onClick={toggleIsMenuVisible}>☰</button>
-      <h1>EventEdge</h1>
-      <Link to='/'><a>Login</a></Link>
-    </nav>
-    {isMenuVisible && <SlidingMenu close={toggleIsMenuVisible} />}
-  </>
-}
+  return (
+    <>
+      <nav className={styles.navigationStrip}>
+        <button className={styles.burgerButton} onClick={toggleIsMenuVisible}>
+          ☰
+        </button>
+        <h1>EventEdge</h1>
+        <Link to="/">
+          <a>Login</a>
+        </Link>
+      </nav>
+      {isMenuVisible && <SlidingMenu close={toggleIsMenuVisible} />}
+    </>
+  );
+};
--- front-end/src/components/navigation/sliding-menu/sliding-menu.jsx
+++ front-end/src/components/navigation/sliding-menu/sliding-menu.jsx
@@ -2,5 +2,7 @@ import React from 'react';
 import styles from './sliding-menu.module.css';
 
 export const SlidingMenu = React.memo(({close}) => (
-  <div className={styles.slidingMenu}>yoo <button onClick={close}>close</button></div>
+  <div className={styles.slidingMenu}>
+    yoo <button onClick={close}>close</button>
+  </div>
 ));
