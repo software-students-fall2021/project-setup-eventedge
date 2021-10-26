9fd5fa4603228f340c8ef5c8602ece643be44cd4
nedjulius <julius.nedzinskas@gmail.com>
2021-10-26 14:21:04 -0400

    Remove mock data

--- front-end/src/lib/mock-data/index.js
+++
@@ -1,6 +0,0 @@
-import chatMessages from './chat-messages.json';
-import chats from './chats.json';
-import events from './events.json';
-import users from './users.json';
-
-export {chatMessages, chats, events, users};
