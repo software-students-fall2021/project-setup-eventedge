4b9d7e866d846b76a94a5161967d0e5994d292a1
nedjulius <julius.nedzinskas@gmail.com>
2021-10-26 00:27:27 -0400

    Add send message modal

--- front-end/src/components/modal-registry/modal-registry.jsx
+++ front-end/src/components/modal-registry/modal-registry.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {useModalContext} from '../../lib/context/modal';
 import {Modal} from '../modal';
+import {SendMessage} from '../send-message';
 
 // for testing purposes. delete later
 const TestComp = () => <div>test</div>;
 
 // {id: string; component: () => React.Component}
-const REGISTERED_MODALS = [{id: 'test', component: () => <TestComp />}];
+const REGISTERED_MODALS = [
+  {id: 'test', component: () => <TestComp />},
+  {id: 'sendMessage', title: 'Send message', component: () => <SendMessage />},
+];
 
 export const ModalRegistry = () => {
   const {hideModal, visibleModals} = useModalContext();
