a56f4ac223ca97dcd0eaf951c6f80cabc926f00b
nedjulius <julius.nedzinskas@gmail.com>
2021-10-27 01:42:39 -0400

    Reuse auth-service

--- front-end/src/components/navigation/navigation.jsx
+++ front-end/src/components/navigation/navigation.jsx
@@ -2,17 +2,13 @@ import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import {SlidingMenu} from './sliding-menu';
 import styles from './navigation.module.css';
+import {authService} from '../../lib/services/auth-service';
 
 export const Navigation = () => {
   const [isMenuVisible, setIsMenuVisible] = useState(false);
 
   const setIsMenuVisibleToTrue = () => setIsMenuVisible(true);
   const setIsMenuVisibleToFalse = () => setIsMenuVisible(false);
-
-  const logOut = () => {
-    localStorage.removeItem('username');
-    window.location = '/';
-  };
 
   return (
     <>
@@ -25,8 +21,8 @@ export const Navigation = () => {
         </button>
         <h1>EventEdge</h1>
         <Link to="/login">
-          {localStorage.getItem('username') ? (
-            <a className={styles.link} onClick={logOut}>
+          {authService().isUserLoggedIn() ? (
+            <a className={styles.link} onClick={authService().logout}>
               Log Out
             </a>
           ) : (
