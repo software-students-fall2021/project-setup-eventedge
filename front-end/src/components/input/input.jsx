7f1ea30eed3456f3ae44c138fb656eabe915dcff
mnu213 <mnu213@nyu.edu>
2021-10-26 20:59:50 -0400

    Add registration page and Input Component

--- front-end/src/components/input/input.jsx
+++ front-end/src/components/input/input.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const Input = (props) => (
-    <div>
-      <input type="text" placeholder={props.placeholder}/>
-    </div>
-  );
+  <div>
+    <input type="text" placeholder={props.placeholder} />
+  </div>
+);
