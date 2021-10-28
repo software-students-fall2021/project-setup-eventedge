64c14f1b033d6924fb95954c4e000790c757fc21
nedjulius <julius.nedzinskas@gmail.com>
2021-10-28 16:49:03 -0400

    Add post service hook

--- front-end/src/lib/services/use-service.js
+++ front-end/src/lib/services/use-service.js
@@ -1,9 +1,36 @@
 import {useEffect, useState} from 'react';
 
+const usePromiseStates = (initialLoading, initialError, initialData) => {
+  const [isLoading, setIsLoading] = useState(initialLoading);
+  const [isError, setIsError] = useState(initialError);
+  const [data, setData] = useState(initialData);
+
+  return {isLoading, isError, data, setIsLoading, setIsError, setData};
+};
+
+export const usePostService = (promise) => {
+  const {isLoading, isError, data, setIsLoading, setIsError, setData} =
+    usePromiseStates(false, false, null);
+
+  const post = async (args) => {
+    try {
+      setIsLoading(true);
+      const data = await promise(args);
+      setData(data);
+      setIsLoading(false);
+    } catch (e) {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return {post, isLoading, isError, data};
+};
+
 export const useService = (method) => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-  const [data, setData] = useState(null);
+  const {isLoading, isError, data, setIsLoading, setIsError, setData} =
+    usePromiseStates(true, false, null);
 
   useEffect(async () => {
     try {
