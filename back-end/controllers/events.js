d15647cb5d04db0b82412cab0746c09af722471d
fm1539 <fm1539@nyu.edu>
2021-11-03 03:31:40 +0000

    revised acceptPending and declinePending

--- back-end/controllers/events.js
+++ back-end/controllers/events.js
@@ -1,9 +1,31 @@
+const {request} = require('./axios');
+const DateGenerator = require('random-date-generator')
+
+
 const acceptPending = (req, res) => {
-  res.send('event accepted');
+    request()
+    .post('/events.json')
+    .then((data) => {
+      res.status(200).json({'message': "event accepted", "event": data});
+    })
+    .catch((e) => {
+      console.error(e);
+      // mockaroo limit reached
+      res.status(200).json({id: generateRandomInt(0, 100), chatName: req.body.chatName});
+    });
 };
 
 const declinePending = (req, res) => {
-  res.send('event declined');
+    request()
+    .post('/chats.json')
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((e) => {
+      console.error(e);
+      // mockaroo limit reached
+      res.status(200).json({id: generateRandomInt(0, 100), chatName: req.body.chatName});
+    });
 };
 
 module.exports = {
