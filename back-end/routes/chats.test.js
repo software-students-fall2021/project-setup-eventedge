5c408ab5a5cb796391958c7c04234804c11e0206
nedjulius <julius.nedzinskas@gmail.com>
2021-11-04 02:07:02 -0400

    Add test suite for chats routes

--- back-end/routes/chats.test.js
+++ back-end/routes/chats.test.js
@@ -1,55 +1,74 @@
 const express = require('express');
 const request = require('supertest');
-const {assert} = require('chai');
+const {expect} = require('chai');
 const chatsRoutes = require('./chats');
 const sinon = require('sinon');
-const axiosWrapper = require('../controllers/axios');
+const axios = require('axios');
 const {CHATS: FALLBACK_CHATS} = require('../mock-data/chat');
+const {USERS: FALLBACK_USERS} = require('../mock-data/user');
 
 const CHATS = [
   {id: 1, chatName: 'Hello'},
   {id: 2, chatName: 'World'},
 ];
 
+const USERS = [
+  {id: 1, username: 'user'},
+  {id: 2, username: 'secondUser'},
+];
+
+const stubAxios = (requestReturn) =>
+  sinon.stub(axios, 'create').returns({request: requestReturn});
+
 describe('chats route', () => {
   let app;
-  let axiosWrapperStub;
 
   beforeEach(() => {
     app = express();
     app.use('/chats', chatsRoutes);
-    axiosWrapperStub = sinon.stub(axiosWrapper, 'request').callsFake(() => {console.log('stub')});
-    process.env.MOCKAROO_API_KEY = 'testKey';
   });
 
   afterEach(() => {
-    axiosWrapperStub.restore();
-    delete process.env.MOCKAROO_API_KEY;
+    sinon.restore();
   });
 
-  describe('/chats', () => {
-    it('should fetch Mockaroo', () => {
-      axiosWrapperStub.returns(Promise.resolve(CHATS));
+  describe('GET /chats', () => {
+    it('should fetch Mockaroo', async () => {
+      stubAxios(() => Promise.resolve({data: CHATS}));
 
-      request(app)
-        .get('/chats')
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .then((res) => {
-          assert.deepEqual(res, CHATS);
-        });
+      const response = await request(app).get('/chats');
+
+      expect(response.status).to.equal(200);
+      expect(response.body).to.deep.equal(CHATS);
     });
 
-    it('should return data from mocks if Mockaroo is unavailable', () => {
-      axiosWrapperStub.returns(Promise.reject());
+    it('should return data from mocks if Mockaroo is unavailable', async () => {
+      stubAxios(() => Promise.reject('test error'));
 
-      request(app)
-        .get('/chats')
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .then((res) => {
-          assert.deepEqual(res, FALLBACK_CHATS);
-        });
-    })
+      const response = await request(app).get('/chats');
+
+      expect(response.status).to.equal(200);
+      expect(response.body).to.deep.equal(FALLBACK_CHATS);
+    });
+  });
+
+  describe('GET /chats/:id/members', () => {
+    it('should fetch Mockaroo', async () => {
+      stubAxios(() => Promise.resolve({data: USERS}));
+
+      const response = await request(app).get('/chats/1/members');
+
+      expect(response.status).to.equal(200);
+      expect(response.body).to.deep.equal(USERS);
+    });
+
+    it('should return data from mocks if Mockaroo is unavailable', async () => {
+      stubAxios(() => Promise.reject('test error'));
+
+      const response = await request(app).get('/chats/1/members');
+
+      expect(response.status).to.equal(200);
+      expect(response.body).to.deep.equal(FALLBACK_USERS);
+    });
   });
 });
