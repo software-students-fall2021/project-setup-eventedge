e0f6032ca6d105886324ab703fd214f4a092853f
nedjulius <julius.nedzinskas@gmail.com>
2021-11-08 23:26:27 -0500

    Format

--- back-end/routes/events.test.js
+++ back-end/routes/events.test.js
@@ -7,57 +7,57 @@ const axios = require('axios');
 const {EVENTS: FALLBACK_EVENTS, EVENTS} = require('../mock-data/events');
 
 const stubAxios = (requestReturn) =>
-    sinon.stub(axios, 'create').returns({request: requestReturn});
+  sinon.stub(axios, 'create').returns({request: requestReturn});
 
-    describe('chat route', () => {
-        let app;
+describe('chat route', () => {
+  let app;
 
-        beforeEach(() => {
-            app = express();
-            app.use('/events', eventsRoutes);
-        });
+  beforeEach(() => {
+    app = express();
+    app.use('/events', eventsRoutes);
+  });
 
-        afterEach(() => {
-            sinon.restore();
-        });
+  afterEach(() => {
+    sinon.restore();
+  });
 
-        describe('GET /events', () => {
-            it('should fetch Mockaroo', async () => {
-                stubAxios(() => Promise.resolve({data: EVENTS}));
+  describe('GET /events', () => {
+    it('should fetch Mockaroo', async () => {
+      stubAxios(() => Promise.resolve({data: EVENTS}));
 
-                const response = await request(app).get('/events');
+      const response = await request(app).get('/events');
 
-                expect(response.status).to.equal(200);
-                expect(response.body).to.deep.equal(EVENTS);
-            });
-
-            it('should return data from mock data if Mockaroo is unavailable', async () => {
-                stubAxios(() => Promise.reject('test error'));
-
-                const response = await request(app).get('/events');
-
-                expect(response.status).to.equal(200);
-                expect(response.body).to.deep.equal(FALLBACK_EVENTS);
-            });
-        });
-
-        describe('GET /events/pending', () => {
-            it('should fetch Mockaroo', async () => {
-                stubAxios(() => Promise.resolve({data: EVENTS}));
-
-                const response = await request(app).get('/events/pending');
-
-                expect(response.status).to.equal(200);
-                expect(response.body).to.deep.equal(EVENTS);
-            });
-
-            it('should return data from mock data if Mockaroo is unavailable', async () => {
-                stubAxios(() => Promise.reject('test error'));
-
-                const response = await request(app).get('/events/pending');
-
-                expect(response.status).to.equal(200);
-                expect(response.body).to.deep.equal(FALLBACK_EVENTS);
-            });
-        })
+      expect(response.status).to.equal(200);
+      expect(response.body).to.deep.equal(EVENTS);
     });
+
+    it('should return data from mock data if Mockaroo is unavailable', async () => {
+      stubAxios(() => Promise.reject('test error'));
+
+      const response = await request(app).get('/events');
+
+      expect(response.status).to.equal(200);
+      expect(response.body).to.deep.equal(FALLBACK_EVENTS);
+    });
+  });
+
+  describe('GET /events/pending', () => {
+    it('should fetch Mockaroo', async () => {
+      stubAxios(() => Promise.resolve({data: EVENTS}));
+
+      const response = await request(app).get('/events/pending');
+
+      expect(response.status).to.equal(200);
+      expect(response.body).to.deep.equal(EVENTS);
+    });
+
+    it('should return data from mock data if Mockaroo is unavailable', async () => {
+      stubAxios(() => Promise.reject('test error'));
+
+      const response = await request(app).get('/events/pending');
+
+      expect(response.status).to.equal(200);
+      expect(response.body).to.deep.equal(FALLBACK_EVENTS);
+    });
+  });
+});
