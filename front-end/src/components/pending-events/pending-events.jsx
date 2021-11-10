a495729b8665e88b7fa75273a3dccf25edd1bca0
nedjulius <julius.nedzinskas@gmail.com>
2021-11-10 09:44:53 -0500

    Add accept/decline post to pending events

--- front-end/src/components/pending-events/pending-events.jsx
+++ front-end/src/components/pending-events/pending-events.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styles from './pending-events.module.css';
 import {useEventService} from '../../lib/services/event-service';
 
 export const PendingEvents = () => {
-  const {isLoading, isError, data} = useEventService.useMyEvents();
+  const {data: eventData, post} = useEventService.useAcceptEvent();
+  const {isLoading, isError, data, setData} = useEventService.useMyPendingEvents();
+
+  useEffect(() => {
+    if (data && eventData) {
+      setData((prevData) => prevData.filter(event => event.id !== eventData.event.id))
+    }
+  }, [eventData])
+
+  const acceptEvent = (id, accept = true) => async () => await post({id, accept});
 
   const mapPendingEvents = isLoading ? (
     <p>Loading...</p>
@@ -12,8 +21,8 @@ export const PendingEvents = () => {
       <div className={styles.box} key={id}>
         <p>Name: {title}</p>
         <p>Date/Time: {date}</p>
-        <button className={styles.acceptButton}>Accept</button>
-        <button className={styles.declineButton}>Decline</button>
+        <button className={styles.acceptButton} onClick={acceptEvent(id)}>Accept</button>
+        <button className={styles.declineButton} onClick={acceptEvent(id, false)}>Decline</button>
       </div>
     ))
   );
