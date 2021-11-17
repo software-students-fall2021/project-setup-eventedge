14e671778fdd894c5513eeae5f3678fa97136224
Farhan Mashud <fm1539@nyu.edu>
2021-11-16 20:05:10 -0500

    Modified changes to events based on schema model

--- back-end/controllers/events.js
+++ back-end/controllers/events.js
@@ -1,79 +1,55 @@
-//const {request} = require('./axios');
-const { Events } = require('../models/Event')
-const { Users } = require('../models/User')
-//const {EVENTS: fakeEventsData} = require('../mock-data/events');
-//const generateRandomInt = require('../utils/generate-random-int');
-//const { usersService } = require('../../front-end/src/lib/services/users-service');
+const User = require('../models/User');
 
-const acceptPending = (req, res) => { //assuming events array in user schema contains both pending and accepted
-  Users.findOne({username: req.body.username}, (err, foundUser) => {
+const acceptPending = (req, res) => {
+  //assuming events array in user schema contains both pending and accepted
+  User.findOne({username: req.body.username}, (err, foundUser) => {
     if (err) console.log(err);
     else {
-      for (let i = 0; i < foundUser.events.length; i++) {
-        if (foundUser.events[i] == req.body.eventId) {
-          Events.findOne({id: req.body.eventId}, (err, foundEvent) => {
-            if (err) console.log(err);
-            else {
-              foundEvent.isPending = false
-              foundEvent.save()
-            }
-          })
+      for (let i = 0; i < foundUser.pendingEvents.length; i++) {
+        if (foundUser.pendingEvents[i] == req.body.eventId) {
+          acceptedEvents.push(req.body.eventId)
+          pendingEvents.splice(i, 1)
+          break
         }
       }
+      foundUser.save();
+      res.status(200).json(foundUser);
     }
-  })
+  });
+};
 
-}
-  
-const declinePending = (req, res) => { //assuming events array in user schema contains both pending and accepted
-  Users.findOne({username: req.body.username}, (err, foundUser) => {
+const declinePending = async (req, res) => {
+  //assuming events array in user schema contains both pending and accepted
+  User.findOne({username: req.body.username}, (err, foundUser) => {
     if (err) res.send(err);
     else {
       for (let i = 0; i < foundUser.events.length; i++) {
         if (foundUser.events[i] == req.body.eventId) {
-          foundUser.events.splice(i, 1)
-          foundUser.save()
-          break
-        } 
+          foundUser.pendingEvents.splice(i, 1);
+          foundUser.save();
+          break;
+        }
       }
-      res.status(200).json(foundUser)
+      res.status(200).json(foundUser);
     }
-  })
-}
+  });
+};
 
-const getPendingEvents = async (req, res) => { //assuming events array in user schema contains both pending and accepted
-  let pendingEvents = []
-  Users.findOne({username: req.session.username}, (err, foundUser) => {
+const getPendingEvents = async (req, res) => {
+  //assuming events array in user schema contains both pending and accepted
+  User.findOne({username: req.body.user.id}, (err, foundUser) => {
     if (err) console.log(err);
-    else {
-      foundUser.events.forEach(eventId => {
-        Events.findOne({id: eventId}, (err, foundEvent) => {
-          if (err) console.log(err);
-          else {
-            if (foundEvent.isPending) pendingEvents.push(foundEvent)
-          }
-        })
-      });
-      res.status(200).json(pendingEvents)
-    }
-  })
-  }
+    else res.status(200).json(foundUser.pendingEvents);
+  });
+};
 
-const getAllUserEvents = async (req, res) => {
-    Users.findOne({username: req.session.username}, (err, foundUser) => {
-      if (err) console.log(err);
-      else {
-        res.status(200).json(foundUser.events)
-      }
-    })
-  }
+const getAllEvents = async (req, res) => {
+  User.findOne({username: req.body.user.id}, (err, foundUser) => {
+    if (err) console.log(err);
+    else res.status(200).json(foundUser.acceptedEvents);
+  });
+};
 
-const getAllEventsInChat = async (req, res) => {
-    Events.find({chatId: req.params.id}, (err, foundEvents) => { //chatId passed into param?
-      if (err) console.log(err);
-      else res.status(200).json(foundEvents)
-    })
-}
 
 const createEvent = async (req, res) => {
   // data from form
@@ -93,6 +69,5 @@ module.exports = {
   declinePending,
   getPendingEvents,
   createEvent,
-  getAllUserEvents,
-  getAllEventsInChat
+  getAllEvents
 };
