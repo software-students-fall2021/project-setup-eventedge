421824a967d0a7fb5a1a356a85e0a0c8f57ae106
nedjulius <julius.nedzinskas@gmail.com>
2021-11-18 16:00:00 -0500

    Modify chat and event models

--- back-end/models/Chat.js
+++ back-end/models/Chat.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
 const chatSchema = new mongoose.Schema({
-  name: String,
-  latestEvent: String,
-  users: Array,
-  messages: Array,
+  name: {type: String, required: true},
+  users: {type: Array, default: []},
+  messages: {type: Array, default: []},
 });
 
 module.exports = mongoose.model('Chat', chatSchema);
--- back-end/models/Event.js
+++ back-end/models/Event.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const eventSchema = new mongoose.Schema({
-  eventName: String,
-  eventDate: Date,
-  eventTime: String,
-  location: String,
-  eventDescription: String,
-  chatId: String,
+  eventName: {type: String, required: [true, "can't be blank"]},
+  eventDate: {type: Date, required: true},
+  eventTime: {type: String, required: true},
+  location: {type: String, required: true},
+  eventDescription: {type: String, required: true},
+  chatId: {type: String, required: true},
 });
 
 module.exports = mongoose.model('Event', eventSchema);
