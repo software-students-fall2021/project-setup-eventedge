903ee091121e88624dc8d847c57c6699d96488ae
mnu213 <mnu213@nyu.edu>
2021-11-18 22:05:08 -0500

    add createEvent query

--- back-end/controllers/events.js
+++ back-end/controllers/events.js
@@ -1,77 +1,92 @@
-const {request} = require('./axios');
-const {EVENTS: fakeEventsData} = require('../mock-data/events');
-
-const acceptPending = (req, res) =>
-  request()
-    .post('/events.json')
-    .then((data) => {
-      res
-        .status(200)
-        .json({message: 'accepted', event: {...data, id: req.body.id}});
-    })
-    .catch((e) => {
-      console.error(e);
-      res.status(200).json({
-        message: 'accepted',
-        event: {
-          ...fakeEventsData[0],
-          id: req.body.id,
-        },
-      });
-    });
-
-const declinePending = (req, res) =>
-  request()
-    .post('/events.json')
-    .then((data) => {
-      res
-        .status(200)
-        .json({message: 'declined', event: {...data, id: req.body.id}});
-    })
-    .catch((e) => {
-      console.error(e);
-      res.status(200).json({
-        message: 'declined',
-        event: {
-          ...fakeEventsData[0],
-          id: req.body.id,
-        },
-      });
-    });
-
-const getPendingEvents = async (_, res) =>
-  request()
-    .get('/events.json')
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((e) => {
-      console.error(e);
-      res.send(fakeEventsData);
-    });
-
-const getAllEvents = async (_, res) =>
-  request()
-    .get('/events.json')
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((e) => {
-      console.error(e);
-      res.send(fakeEventsData);
-    });
-
-const createEvent = async (req, res) => {
-  // data from form
-  const {name, date, time, location, description} = req.body;
-
-  res.status(200).json({
-    name,
-    date,
-    time,
-    location,
-    description,
+const Event = require('../models/Event');
+const User = require('../models/User');
+const Chat = require('../models/Chat');
+const acceptPending = (req, res) => {
+  //assuming events array in user schema contains both pending and accepted
+  User.findOne({username: req.body.username}, (err, foundUser) => {
+    if (err) console.log(err);
+    else {
+      for (let i = 0; i < foundUser.pendingEvents.length; i++) {
+        if (foundUser.pendingEvents[i] == req.body.eventId) {
+          acceptedEvents.push(req.body.eventId)
+          pendingEvents.splice(i, 1)
+          break
+        }
+      }
+      foundUser.save();
+      res.status(200).json(foundUser);
+    }
   });
+};
+const declinePending = async (req, res) => {
+  //assuming events array in user schema contains both pending and accepted
+  User.findOne({username: req.body.username}, (err, foundUser) => {
+    if (err) res.send(err);
+    else {
+      for (let i = 0; i < foundUser.events.length; i++) {
+        if (foundUser.events[i] == req.body.eventId) {
+          foundUser.pendingEvents.splice(i, 1);
+          foundUser.save();
+          break;
+        }
+      }
+      res.status(200).json(foundUser);
+    }
+  });
+};
+const getPendingEvents = async (req, res) => {
+  //assuming events array in user schema contains both pending and accepted
+  try {
+    const user = await User.findById(req.body.user.id);
+    const events = await Event.find({ '_id': { $in: user.pendingEvents } });
+    res.status(200).json(events);
+  } catch (e) {
+    console.error(e)
+  }
+};
+
+const getAllEvents = async (req, res) => {
+  try {
+    const user = await User.findById(req.body.user.id);
+    const events = await Event.find({ '_id': { $in: user.acceptedEvents } });
+    res.status(200).json(events);
+  } catch (e) {
+    console.error(e)
+  }
+};
+const createEvent = async (req, res) => {
+  try {
+    const {name, date, time, location, description, chatId} = req.body;
+
+    const event = await Event.insertOne(
+      {
+        eventName: name, 
+        eventDate: date,
+        eventTime: time,
+        location: location,
+        eventDescription: description,
+        chatId: chatId
+      }
+    )
+    const chat = await Chat.findById(chatId)
+    const members = await User.find({_id: {$in: chat.users}})
+    for (let i = 0; i < members.length; i++) {
+      members[i].pendingEvents.push(event.id)
+      members[i].save()
+    }
+
+
+    res.status(200).json({
+      name,
+      date,
+      time,
+      location,
+      description,
+      chatId,
+    });
+  } catch (e) {
+    console.error(e)
+  }
 };
 
 module.exports = {
