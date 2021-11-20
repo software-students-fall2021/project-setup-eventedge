3022326f6d6731ead6903bb9985261d109ac2a6c
mnu213 <mnu213@nyu.edu>
2021-11-20 11:45:39 -0500

    update events.js

--- back-end/controllers/events.js
+++ back-end/controllers/events.js
@@ -1,59 +1,7 @@
 const Event = require('../models/Event');
 const User = require('../models/User');
 const Chat = require('../models/Chat');
-const acceptPending = (req, res) => {
-  //assuming events array in user schema contains both pending and accepted
-  User.findOne({username: req.body.username}, (err, foundUser) => {
-    if (err) console.log(err);
-    else {
-      for (let i = 0; i < foundUser.pendingEvents.length; i++) {
-        if (foundUser.pendingEvents[i] == req.body.eventId) {
-          acceptedEvents.push(req.body.eventId)
-          pendingEvents.splice(i, 1)
-          break
-        }
-      }
-      foundUser.save();
-      res.status(200).json(foundUser);
-    }
-  });
-};
-const declinePending = async (req, res) => {
-  //assuming events array in user schema contains both pending and accepted
-  User.findOne({username: req.body.username}, (err, foundUser) => {
-    if (err) res.send(err);
-    else {
-      for (let i = 0; i < foundUser.events.length; i++) {
-        if (foundUser.events[i] == req.body.eventId) {
-          foundUser.pendingEvents.splice(i, 1);
-          foundUser.save();
-          break;
-        }
-      }
-      res.status(200).json(foundUser);
-    }
-  });
-};
-const getPendingEvents = async (req, res) => {
-  //assuming events array in user schema contains both pending and accepted
-  try {
-    const user = await User.findById(req.body.user.id);
-    const events = await Event.find({ '_id': { $in: user.pendingEvents } });
-    res.status(200).json(events);
-  } catch (e) {
-    console.error(e)
-  }
-};
 
-const getAllEvents = async (req, res) => {
-  try {
-    const user = await User.findById(req.body.user.id);
-    const events = await Event.find({ '_id': { $in: user.acceptedEvents } });
-    res.status(200).json(events);
-  } catch (e) {
-    console.error(e)
-  }
-};
 const createEvent = async (req, res) => {
   try {
     const {name, date, time, location, description, chatId} = req.body;
@@ -90,9 +38,5 @@ const createEvent = async (req, res) => {
 };
 
 module.exports = {
-  acceptPending,
-  declinePending,
-  getPendingEvents,
   createEvent,
-  getAllEvents,
 };
