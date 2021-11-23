69ee2ae260849c1ddf2985dbded2737549feedf9
nedjulius <julius.nedzinskas@gmail.com>
2021-11-23 04:07:16 -0500

    Add error handler middleware

--- back-end/app.js
+++ back-end/app.js
@@ -4,6 +4,7 @@ const http = require('http');
 const cors = require('cors');
 const passport = require('passport');
 const {jwtStrategy} = require('./configs/jwt-strategy');
+const errorHandler = require('./middlewares/error-handler');
 const authRoutes = require('./routes/auth');
 const eventsRoutes = require('./routes/events');
 const chatsRoutes = require('./routes/chats');
@@ -27,6 +28,8 @@ app.use('/events', eventsRoutes);
 app.use('/chats', chatsRoutes);
 app.use('/events', eventsRoutes);
 app.use('/users', usersRoutes);
+
+app.use(errorHandler);
 
 const server = http.createServer(app);
 
