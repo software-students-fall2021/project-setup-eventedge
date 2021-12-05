28fb7002a4adc1a9d00872f6e806ef426facebc2
nedjulius <julius.nedzinskas@gmail.com>
2021-12-05 15:23:38 -0500

    Format

--- front-end/src/components/landing-page/landing-page.jsx
+++ front-end/src/components/landing-page/landing-page.jsx
@@ -25,76 +25,76 @@ export const LandingPage = () => {
   };
 
   return (
-      <div>
-        <div className={styles.container}>
-          <h1 className={styles.title}>Plan Your Next Event</h1>
-          <img className={styles.gif} src={landingGif} />
-          <video autoPlay muted loop className={styles.video}>
-            <source src="videos/lunchVid.mp4" type="video/mp4" />
-          </video>
-          <div className={styles.buttons}>
-            <Link className={styles.link} to="/login">
-              <button className={styles.button}> Login</button>
-            </Link>
+    <div>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Plan Your Next Event</h1>
+        <img className={styles.gif} src={landingGif} />
+        <video autoPlay muted loop className={styles.video}>
+          <source src="videos/lunchVid.mp4" type="video/mp4" />
+        </video>
+        <div className={styles.buttons}>
+          <Link className={styles.link} to="/login">
+            <button className={styles.button}> Login</button>
+          </Link>
 
-            <Link className={styles.link} to="/register">
-              <button className={styles.button}> Register</button>
-            </Link>
-          </div>
-          <img
-            onClick={() => (window.location = '/#next')}
-            className={styles.arrow}
-            src="imgs/arrow.svg"
-          />
+          <Link className={styles.link} to="/register">
+            <button className={styles.button}> Register</button>
+          </Link>
         </div>
-        <div id="next" className={styles.secondSection}>
-          <h4>
-            EventEdge came out of wanting to connect communities together in a
-            new way.
-          </h4>
-          <h4>When using EventEdge, you can</h4>
-          <ul>
-            <li>create group chats</li>
-            <li>talk, create, and plan events with your peers</li>
-            <li>see all your events in one place</li>
-          </ul>
-          <h4>
-            To explore more, register! To get back to your chats and events,
-            login!
-          </h4>
-
-          <div>
-            <h1>Questions or issues? Email us!</h1>
-            <label>From:</label> <br />
-            <input
-              onChange={onFromChange}
-              placeholder="Your Email Address"
-            />{' '}
-            <br />
-            <br />
-            <label>Subject: </label> <br />
-            <input onChange={onSubjectChange} /> <br />
-            <br />
-            <label>Message:</label> <br />
-            <textarea
-              rows="10"
-              cols="30"
-              onChange={onMessageChange}
-              onKeyPress={onEnterPress}
-            />
-            <br />
-            <br />
-            <button onClick={onSubmit}>Send</button> <br />
-            <br />
-          </div>
-        </div>
-
-        <marquee>
-          Thank you for visiting EventEdge. We hope you can use the product to
-          boost efficiency when it comes to planning your future events. If you
-          find us to be a good service, let your friends know about EventEdge as
-          well. For any further assistance, email eventedge@gmail.com.
-        </marquee>
+        <img
+          onClick={() => (window.location = '/#next')}
+          className={styles.arrow}
+          src="imgs/arrow.svg"
+        />
       </div>
+      <div id="next" className={styles.secondSection}>
+        <h4>
+          EventEdge came out of wanting to connect communities together in a new
+          way.
+        </h4>
+        <h4>When using EventEdge, you can</h4>
+        <ul>
+          <li>create group chats</li>
+          <li>talk, create, and plan events with your peers</li>
+          <li>see all your events in one place</li>
+        </ul>
+        <h4>
+          To explore more, register! To get back to your chats and events,
+          login!
+        </h4>
+
+        <div>
+          <h1>Questions or issues? Email us!</h1>
+          <label>From:</label> <br />
+          <input
+            onChange={onFromChange}
+            placeholder="Your Email Address"
+          />{' '}
+          <br />
+          <br />
+          <label>Subject: </label> <br />
+          <input onChange={onSubjectChange} /> <br />
+          <br />
+          <label>Message:</label> <br />
+          <textarea
+            rows="10"
+            cols="30"
+            onChange={onMessageChange}
+            onKeyPress={onEnterPress}
+          />
+          <br />
+          <br />
+          <button onClick={onSubmit}>Send</button> <br />
+          <br />
+        </div>
+      </div>
+
+      <marquee>
+        Thank you for visiting EventEdge. We hope you can use the product to
+        boost efficiency when it comes to planning your future events. If you
+        find us to be a good service, let your friends know about EventEdge as
+        well. For any further assistance, email eventedge@gmail.com.
+      </marquee>
+    </div>
   );
 };
