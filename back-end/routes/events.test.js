82a41d19a2474a6b8764020b0e72611649b48918
nedjulius <julius.nedzinskas@gmail.com>
2021-12-07 04:04:43 -0500

    Update events test

--- back-end/routes/events.test.js
+++ back-end/routes/events.test.js
@@ -1,238 +1,199 @@
-const express = require('express');
-const request = require('supertest');
-const {expect} = require('chai');
-const mongoMock = require('../utils/test/mongodb-mock');
-const eventsRoutes = require('./events');
+const app = require('../app');
+const request = require('supertest')(app);
+const {expect, should} = require('chai');
 const User = require('../models/User');
 const Event = require('../models/Event');
-const sinon = require('sinon');
-const passport = require('passport');
+const generateMongoObjectId = require('../utils/test/generate-mongo-object-id');
+const Chance = require('chance');
+
+const chance = new Chance();
+
+const authUsername = 'authUsername';
+const authPassword = 'authPassword';
+
+const generateRandomEventData = () => ({
+  name: chance.sentence({words: 2}),
+  date: chance.date({string: true}),
+  time: '10:00PM',
+  location: chance.city(),
+  description: chance.sentence({words: 10}),
+  chatId: generateMongoObjectId(),
+});
 
 describe('events route', () => {
-  let app;
+  let authHeader;
+  let authUserId;
 
-  before(async () => {
-    await mongoMock.connectToDatabase();
-  });
-
-  beforeEach(() => {
-    app = express();
-    app.use(express.urlencoded({extended: true}));
-    app.use(express.json());
-    app.use('/events', eventsRoutes);
-  });
-
-  afterEach(async () => {
-    await mongoMock.clearDatabase();
-  });
-
-  after(async () => await mongoMock.closeDatabase());
-
-  describe('POST /events/pending/accept', () => {
-    it('should remove specified event from pending array and add to accepted', async () => {
-      const {eventId1} = await Event.create({
-        name: 'Exam 2',
-        date: Date(2021 - 12 - 10),
-        time: '8:00PM',
-        location: 'School',
-        description: 'The worst exam of my life',
-        chatId: '3',
-      });
-      const {eventId2} = await Event.create({
-        name: 'Exam 3',
-        date: Date(2021 - 12 - 20),
-        time: '4:00PM',
-        location: 'School',
-        description: 'Another exam',
-        chatId: '3',
-      });
-
-      const {eventId3} = await Event.create({
-        name: 'Restaurant hangout',
-        date: Date(2022 - 01 - 01),
-        time: '6:00PM',
-        location: 'NYC',
-        description: 'Having fun with the boiz',
-        chatId: '3',
-      });
-
-      const {userId} = await User.create({
-        username: 'farhan',
-        password:
-          '$2b$10$jvVvHoJg7n52K3Xic2yVueoBp9FhPLx8WdNEJ2pjjS3U3.nwaW2ii',
-        chats: [],
-        pendingEvents: [eventId1, eventId2],
-        acceptedEvents: [eventId3],
-      });
-      sinon
-        .stub(passport, 'authenticate')
-        .callsFake((strategy, options, callback) => {
-          callback(null, {username: userId}, null);
-          return (req, res, next) => {};
-        });
-      const response = await request(app)
-        .post('/events/pending/accept')
-        .send({userId: userId, eventId: eventId1});
-      expect(response.status).to.equal(200);
-      expect(response.body).to.deep.equal({
-        accepted: [eventId2],
-        pending: [eventId3, eventId1],
-      });
+  beforeEach(async () => {
+    const {id} = await User.create({
+      username: authUsername,
+      password: authPassword,
     });
-  });
+    const res = await request
+      .post('/auth/login')
+      .send({username: authUsername, password: authPassword});
 
-  describe('POST /events/pending/decline', () => {
-    it('should remove specified event from pending array ONLY', async () => {
-      const {eventId1} = await Event.create({
-        name: 'Exam 2',
-        date: Date(2021 - 12 - 10),
-        time: '8:00PM',
-        location: 'School',
-        description: 'The worst exam of my life',
-        chatId: '3',
-      });
-      const {eventId2} = await Event.create({
-        name: 'Exam 3',
-        date: Date(2021 - 12 - 20),
-        time: '4:00PM',
-        location: 'School',
-        description: 'Another exam',
-        chatId: '3',
-      });
-
-      const {eventId3} = await Event.create({
-        name: 'Restaurant hangout',
-        date: Date(2022 - 01 - 01),
-        time: '6:00PM',
-        location: 'NYC',
-        description: 'Having fun with the boiz',
-        chatId: '3',
-      });
-
-      const {userId} = await User.create({
-        username: 'farhan',
-        password:
-          '$2b$10$jvVvHoJg7n52K3Xic2yVueoBp9FhPLx8WdNEJ2pjjS3U3.nwaW2ii',
-        chats: [],
-        pendingEvents: [eventId1, eventId2],
-        acceptedEvents: [eventId3],
-      });
-      sinon
-        .stub(passport, 'authenticate')
-        .callsFake((strategy, options, callback) => {
-          callback(null, {username: userId}, null);
-          return (req, res, next) => {};
-        });
-      const response = await request(app)
-        .post('/events/pending/decline')
-        .send({userId: userId, eventId: eventId1});
-      expect(response.status).to.equal(200);
-      expect(response.body).to.deep.equal({
-        accepted: [eventId2],
-        pending: [eventId3],
-      });
-    });
+    authUserId = id;
+    authHeader = {Authorization: `Bearer ${res.body.token}`};
   });
 
   describe('GET /events', () => {
-    it('should get accepted events from MongoDB', async () => {
-      const {eventId1} = await Event.create({
-        name: 'Exam 2',
-        date: Date(2021 - 12 - 10),
-        time: '8:00PM',
-        location: 'School',
-        description: 'The worst exam of my life',
-        chatId: '3',
-      });
-      const {eventId2} = await Event.create({
-        name: 'Exam 3',
-        date: Date(2021 - 12 - 20),
-        time: '4:00PM',
-        location: 'School',
-        description: 'Another exam',
-        chatId: '3',
-      });
+    it('should get empty accepted events if there are not any', async () => {
+      const response = await request.get('/events').set(authHeader);
 
-      const {eventId3} = await Event.create({
-        name: 'Restaurant hangout',
-        date: Date(2022 - 01 - 01),
-        time: '6:00PM',
-        location: 'NYC',
-        description: 'Having fun with the boiz',
-        chatId: '3',
-      });
-      const {userId} = await User.create({
-        username: 'farhan',
-        password:
-          '$2b$10$jvVvHoJg7n52K3Xic2yVueoBp9FhPLx8WdNEJ2pjjS3U3.nwaW2ii',
-        chats: [],
-        pendingEvents: [eventId1, eventId2],
-        acceptedEvents: [eventId3],
-      });
+      expect(response.status).to.equal(200);
+      expect(response.body).to.deep.equal([]);
+    });
 
-      sinon
-        .stub(passport, 'authenticate')
-        .callsFake((strategy, options, callback) => {
-          callback(null, {username: userId}, null);
-          return (req, res, next) => {};
-        });
-      return request
-        .get('/events')
-        .expect(200)
-        .expect((res) => {
-          res.body.acceptedEvents.should.equal(userId.acceptedEvents);
-        });
+    it('should get accepted events in date descending order', async () => {
+      const firstEvent = await Event.create(generateRandomEventData());
+      const secondEvent = await Event.create(generateRandomEventData());
+      const thirdEvent = await Event.create(generateRandomEventData());
+
+      await User.updateOne(
+        {_id: authUserId},
+        {$push: {acceptedEvents: firstEvent.id, pendingEvents: secondEvent.id}}
+      );
+      await User.updateOne(
+        {_id: authUserId},
+        {$push: {acceptedEvents: thirdEvent.id}}
+      );
+
+      const response = await request.get('/events').set(authHeader);
+
+      expect(response.status).to.equal(200);
+      expect(response.body).to.deep.equal([
+        {
+          name: thirdEvent.name,
+          date: thirdEvent.date.toISOString(),
+          chatId: thirdEvent.chatId,
+          id: thirdEvent.id,
+        },
+        {
+          name: firstEvent.name,
+          date: firstEvent.date.toISOString(),
+          chatId: firstEvent.chatId,
+          id: firstEvent.id,
+        },
+      ]);
     });
   });
 
   describe('GET /events/pending', () => {
-    it('should get pending events from MongoDB', async () => {
-      const {eventId1} = await Event.create({
-        name: 'Exam 2',
-        date: Date(2021 - 12 - 10),
-        time: '8:00PM',
-        location: 'School',
-        description: 'The worst exam of my life',
-        chatId: '3',
-      });
-      const {eventId2} = await Event.create({
-        name: 'Exam 3',
-        date: Date(2021 - 12 - 20),
-        time: '4:00PM',
-        location: 'School',
-        description: 'Another exam',
-        chatId: '3',
-      });
+    it('should get empty pending events if there are not any', async () => {
+      const response = await request.get('/events/pending').set(authHeader);
 
-      const {eventId3} = await Event.create({
-        name: 'Restaurant hangout',
-        date: Date(2022 - 01 - 01),
-        time: '6:00PM',
-        location: 'NYC',
-        description: 'Having fun with the boiz',
-        chatId: '3',
-      });
-      const {userId} = await User.create({
-        username: 'farhan',
-        password:
-          '$2b$10$jvVvHoJg7n52K3Xic2yVueoBp9FhPLx8WdNEJ2pjjS3U3.nwaW2ii',
-        chats: [],
-        pendingEvents: [eventId1, eventId2],
-        acceptedEvents: [eventId3],
-      });
+      expect(response.status).to.equal(200);
+      expect(response.body).to.deep.equal([]);
+    });
 
-      sinon
-        .stub(passport, 'authenticate')
-        .callsFake((strategy, options, callback) => {
-          callback(null, {username: userId}, null);
-          return (req, res, next) => {};
-        });
-      return request
-        .get('/events')
-        .expect(200)
-        .expect((res) => {
-          res.body.pendingEvents.should.equal(userId.pendingEvents);
-        });
+    it('should get pending events in date descending order', async () => {
+      const firstEvent = await Event.create(generateRandomEventData());
+      const secondEvent = await Event.create(generateRandomEventData());
+      const thirdEvent = await Event.create(generateRandomEventData());
+
+      await User.updateOne(
+        {_id: authUserId},
+        {$push: {pendingEvents: firstEvent.id, acceptedEvents: secondEvent.id}}
+      );
+      await User.updateOne(
+        {_id: authUserId},
+        {$push: {pendingEvents: thirdEvent.id}}
+      );
+
+      const response = await request.get('/events/pending').set(authHeader);
+
+      expect(response.status).to.equal(200);
+      expect(response.body).to.deep.equal([
+        {
+          name: thirdEvent.name,
+          date: thirdEvent.date.toISOString(),
+          id: thirdEvent.id,
+        },
+        {
+          name: firstEvent.name,
+          date: firstEvent.date.toISOString(),
+          id: firstEvent.id,
+        },
+      ]);
+    });
+  });
+
+  describe('POST /events/pending/accept', () => {
+    it('should accept pending event', async () => {
+      const pendingEvent = await Event.create(generateRandomEventData());
+
+      await User.updateOne(
+        {_id: authUserId},
+        {$push: {pendingEvents: pendingEvent.id}}
+      );
+
+      const response = await request
+        .post('/events/pending/accept')
+        .set(authHeader)
+        .send({eventId: pendingEvent.id});
+
+      const updatedUser = await User.findById(authUserId);
+
+      expect(response.status).to.equal(200);
+      expect(response.body).to.deep.equal({
+        date: pendingEvent.date.toISOString(),
+        id: pendingEvent.id,
+        name: pendingEvent.name,
+      });
+      expect(updatedUser.acceptedEvents).to.deep.equal([pendingEvent.id]);
+      expect(updatedUser.pendingEvents).to.deep.equal([]);
+    });
+
+    it('should return unauthorized error if user is not invited to event', async () => {
+      const pendingEvent = await Event.create(generateRandomEventData());
+
+      const response = await request
+        .post('/events/pending/accept')
+        .set(authHeader)
+        .send({eventId: pendingEvent.id});
+
+      expect(response.status).to.equal(401);
+      expect(response.body).to.deep.equal({error: 'Unauthorized'});
+    });
+  });
+
+  describe('POST /events/pending/decline', () => {
+    it('should decline pending event', async () => {
+      const pendingEvent = await Event.create(generateRandomEventData());
+
+      await User.updateOne(
+        {_id: authUserId},
+        {$push: {pendingEvents: pendingEvent.id}}
+      );
+
+      const response = await request
+        .post('/events/pending/decline')
+        .set(authHeader)
+        .send({eventId: pendingEvent.id});
+
+      const updatedUser = await User.findById(authUserId);
+
+      expect(response.status).to.equal(200);
+      expect(response.body).to.deep.equal({
+        date: pendingEvent.date.toISOString(),
+        id: pendingEvent.id,
+        name: pendingEvent.name,
+      });
+      expect(updatedUser.acceptedEvents).to.deep.equal([]);
+      expect(updatedUser.pendingEvents).to.deep.equal([]);
+    });
+
+    it('should return unauthorized error if user is not invited to event', async () => {
+      const pendingEvent = await Event.create(generateRandomEventData());
+
+      const response = await request
+        .post('/events/pending/decline')
+        .set(authHeader)
+        .send({eventId: pendingEvent.id});
+
+      expect(response.status).to.equal(401);
+      expect(response.body).to.deep.equal({error: 'Unauthorized'});
     });
   });
 });
