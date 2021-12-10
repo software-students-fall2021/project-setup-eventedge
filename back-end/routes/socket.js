f0427b34fc69baf5989a53e44a243b08d68526d2
nedjulius <julius.nedzinskas@gmail.com>
2021-12-10 10:13:48 -0500

    Update socket configuraiton

--- back-end/routes/socket.js
+++ back-end/routes/socket.js
@@ -6,7 +6,7 @@ const createSocket = (server) => {
     cors: {
       origin: '*',
       methods: ['GET', 'POST'],
-      transports: ['websocket', 'polling'],
+      transports: ['polling', 'websocket'],
     },
   });
 
