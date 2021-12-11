aeba94a2cf9ce1bfd53923878be8e3ff6d28efc3
nedjulius <julius.nedzinskas@gmail.com>
2021-12-11 02:09:39 -0500

    Add optimistic updates and improve UI

--- front-end/src/components/pending-events/pending-events.jsx
+++ front-end/src/components/pending-events/pending-events.jsx
@@ -3,8 +3,9 @@ import styles from './pending-events.module.css';
 import {useEventService} from '../../lib/services/event-service';
 import {getDateString} from '../../lib/utils/get-date-string';
 import {useToastContext} from '../../lib/context/toast';
+import {Loader} from '../loader';
 
-export const PendingEvents = () => {
+export const PendingEvents = ({setEventsData}) => {
   const {showSuccessToast} = useToastContext();
   const {data: eventData, post} = useEventService.useAcceptEvent();
   const {isLoading, isError, data, setData} =
@@ -15,6 +16,10 @@ export const PendingEvents = () => {
       setData((prevData) =>
         prevData.filter((event) => event.id !== eventData.id)
       );
+
+      if (eventData.accepted) {
+        setEventsData((prevEventsData) => [eventData, ...prevEventsData]);
+      }
     }
   }, [eventData]);
 
@@ -22,32 +27,49 @@ export const PendingEvents = () => {
     (id, accept = true) =>
     async () => {
       await post({eventId: id, accept});
-      showSuccessToast('Accepted a pending event!');
+      showSuccessToast(accept ? 'Accepted an event!' : 'Declined an event!');
     };
 
+  const areThereNoPendingEvents = data?.length === 0;
+
   const mapPendingEvents = isLoading ? (
-    <p>Loading...</p>
+    <Loader />
   ) : (
     data?.map(({date, name, id}) => (
       <div className={styles.box} key={id}>
-        <p>Name: {name}</p>
-        <p>Date/Time: {getDateString(date)}</p>
-        <button className={styles.acceptButton} onClick={acceptEvent(id)}>
-          Accept
-        </button>
-        <button
-          className={styles.declineButton}
-          onClick={acceptEvent(id, false)}
-        >
-          Decline
-        </button>
+        <p>
+          <strong>Name:</strong> {name}
+        </p>
+        <p>
+          <strong>Date/Time:</strong> {getDateString(date)}
+        </p>
+        <div className={styles.acceptanceButtons}>
+          <button
+            className={`${styles.acceptanceButton} ${styles.accept}`}
+            onClick={acceptEvent(id)}
+          >
+            Accept
+          </button>
+          <button
+            className={`${styles.acceptanceButton} ${styles.decline}`}
+            onClick={acceptEvent(id, false)}
+          >
+            Decline
+          </button>
+        </div>
       </div>
     ))
   );
 
   return (
     <div className={styles.container}>
-      {isError ? <p>An error occured</p> : mapPendingEvents}
+      {isError ? (
+        <p>An error occured</p>
+      ) : areThereNoPendingEvents ? (
+        <p>You have no pending events.</p>
+      ) : (
+        mapPendingEvents
+      )}
     </div>
   );
 };
